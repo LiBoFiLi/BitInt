ab7811d95921fac782a6d5c2a0d34e8fbceaa73c
LiBiLiBO <Dimon240504@gmail.com>
2024-10-17 02:07:44 +0300

    pow time test

--- timetest.cpp
+++ timetest.cpp
@@ -18,8 +18,8 @@ double measureTime(Func f, size_t iterations) {
 double testrand(size_t iterations , int size) {
     return measureTime([&]() {
         BigInt a, b;
-        a.random(size); // Генерация случайного числа
-        b.random(size); // Генерация второго случайного числа
+        a.random(size); 
+        b.random(size); 
     }, iterations);
 }
 
@@ -27,9 +27,9 @@ double testrand(size_t iterations , int size) {
 double testsum(size_t iterations , int size) {
     return measureTime([&]() {
         BigInt a, b;
-        a.random(size); // Генерация случайного числа
-        b.random(size); // Генерация второго случайного числа
-        BigInt c = a + b; // Операция сложения
+        a.random(size); 
+        b.random(size); 
+        BigInt c = a + b; 
     }, iterations);
 }
 
@@ -37,46 +37,65 @@ double testsum(size_t iterations , int size) {
 double testdiff(size_t iterations, int size) {
     return measureTime([&]() {
         BigInt a, b;
-        a.random(size); // Генерация случайного числа
-        b.random(size); // Генерация второго случайного числа
-        BigInt c = a - b; // Операция сложения
+        a.random(size); 
+        b.random(size); 
+        BigInt c = a - b; 
     }, iterations);
 }
 
 double testmul(size_t iterations, int size) {
     return measureTime([&]() {
         BigInt a, b;
-        a.random(size); // Генерация случайного числа
-        b.random(size); // Генерация второго случайного числа
-        BigInt c = a * b; // Операция сложения
+        a.random(size); 
+        b.random(size); 
+        BigInt c = a * b; 
     }, iterations);
 }
 
 double testdiv(size_t iterations, int size) {
     return measureTime([&]() {
         BigInt a, b;
-        a.random(size); // Генерация случайного числа
-        b.random(size); // Генерация второго случайного числа
-        BigInt c = a / b; // Операция сложения
+        a.random(size); 
+        b.random(size); 
+        BigInt c = a / b; 
     }, iterations);
 }
 
+double testrand1(size_t iterations , int size) {
+    return measureTime([&]() {
+        BigInt a, b;
+        a.random(size); 
+        b = 100;  
+    }, iterations);
+}
+
+double testpow(size_t iterations, int size) {
+    return measureTime([&]() {
+        BigInt a, b;
+        a.random(size); 
+        b = 100; 
+        BigInt c = a.pow(b); 
+    }, iterations);
+}
 
 
 int main() {
     size_t iterations = 10000; 
 
     double avgRandTime = testrand(iterations, 32);
+    double avgRandTime1 = testrand1(iterations, 32);
     double avgAddTime = testsum(iterations, 32);
     double avgSubTime = testdiff(iterations, 32);
     double avgMulTime = testmul(iterations, 32);
     double avgDivTime = testdiv(iterations, 32);
+    //double avgPowTime = testpow(iterations - 9900, 32);
 
     cout << "Average time for random generation: " << avgRandTime * 1 << " seconds\n";
     cout << "Average time for addition: " << (avgAddTime - avgRandTime) * 1 << " seconds\n";
     cout << "Average time for subtraction: " << (avgSubTime - avgRandTime) * 1  << " seconds\n";
     cout << "Average time for multiplication: " << (avgMulTime - avgRandTime) * 1 << " seconds\n";
     cout << "Average time for division: " << (avgDivTime - avgRandTime) * 1 << " seconds\n";
+   // cout << "Average time for exp: " << (avgPowTime - avgRandTime1) * 1 << " seconds\n";
 
     return 0;
 }
